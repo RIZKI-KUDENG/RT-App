7e4c709bfc968391cdc8fbf0eab0d48e7c0a5227
M Rizki Arya Nanda <rizkiarya0611@gmail.com>
2026-02-17 16:20:12 +0700

    create migration and model

---
+++ backend/database/migrations/2026_02_17_090225_create_occupants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('occupants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('ktp')->nullable();
+            $table->enum('status', ['permanent', 'contract']);
+            $table->string('phone');
+            $table->boolean('is_married')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('occupants');
+    }
+};
